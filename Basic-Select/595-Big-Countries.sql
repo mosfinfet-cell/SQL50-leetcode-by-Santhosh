00f1605b252d7c03706237ed0f30a8b2b1017569
mosfinfet-cell <mos.finfet@gmail.com>
2026-02-03 17:57:12 +0530

    Create 595-Big-Countries.sql

---
+++ Basic-Select/595-Big-Countries.sql
@@ -0,0 +1,16 @@
+/*
+Problem: 595-Big-Countries
+Link: https://leetcode.com/problems/big-countries/description/?envType=study-plan-v2&envId=top-sql-50
+Difficulty: Easy
+Category: Basic Select
+*/
+
+# Write your MySQL query statement below
+SELECT
+    name,
+    population,
+    area
+FROM
+    World
+WHERE area >= 3000000
+        OR population >= 25000000

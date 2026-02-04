7087b57956329f28a1f48deff5dc9b29298ef36b
mosfinfet-cell <mos.finfet@gmail.com>
2026-02-04 11:54:18 +0530

    Update 620_Not_Boring_Movies.sql

--- Basic-Aggregate-Functions/620_Not_Boring_Movies.sql
+++ Basic-Aggregate-Functions/620_Not_Boring_Movies.sql
@@ -2,7 +2,7 @@
 Problem: 620_Not_Boring_Movies
 Link: https://leetcode.com/problems/not-boring-movies/description/?envType=study-plan-v2&envId=top-sql-50
 Difficulty: Easy
-Category: Basic Aggregate Movies
+Category: Basic Aggregate Functions
 */
 
 

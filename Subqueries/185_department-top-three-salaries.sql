b01e4e5409c6ab636cad86fd5ec12945b0683dd6
mosfinfet-cell <mos.finfet@gmail.com>
2026-02-10 20:47:54 +0530

    Delete Subqueries/185_department-top-three-salaries.sql

--- Subqueries/185_department-top-three-salaries.sql
+++
@@ -1,7 +0,0 @@
-/*
-Problem:185_department-top-three-salaries
-Link: http://leetcode.com/problems/department-top-three-salaries/description/?envType=study-plan-v2&envId=top-sql-50
-Difficulty: Hard
-Category: Subqueries
-*/
-

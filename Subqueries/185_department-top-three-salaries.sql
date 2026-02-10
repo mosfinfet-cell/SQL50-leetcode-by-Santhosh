fcdd32a976214b2659996b5ea934be6643495b2e
mosfinfet-cell <mos.finfet@gmail.com>
2026-02-10 20:36:08 +0530

    Create 185_department-top-three-salaries.sql

---
+++ Subqueries/185_department-top-three-salaries.sql
@@ -0,0 +1,7 @@
+/*
+Problem:185_department-top-three-salaries
+Link: http://leetcode.com/problems/department-top-three-salaries/description/?envType=study-plan-v2&envId=top-sql-50
+Difficulty: Hard
+Category: Subqueries
+*/
+
